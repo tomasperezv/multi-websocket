662b9519d49b7e98af270e802282bc6d0601d774
Tomás Pérez <t@0x101.com>
2017-01-02 13:16:07 +0100

    Adding dependencies bootstrap file.

---
+++ src/multi-websocket.js
@@ -0,0 +1,11 @@
+/**
+ * Encapsulates the different objects that compose the MultiWebsocket library.
+
+ * @namespace MultiWebsocket
+ */
+'use strict';
+
+module.exports = {
+  EventBus: require('./event-bus'),
+  Client: require('./client')
+};
